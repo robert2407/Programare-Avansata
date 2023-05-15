0fd9d7dc41c51c38992cd3ea5bbbe85668949fc0
robert2407 <63233501+robert2407@users.noreply.github.com>
2023-05-15 19:38:08 +0300

    Add files via upload

---
+++ PA/Lab11/Laborator11/src/main/java/C11/Player.java
@@ -0,0 +1,12 @@
+package C11;
+
+public class Player {
+
+    private String name;
+    Player(String name){
+        this.name = name;
+    }
+    public String getName(){
+        return this.name;
+    }
+}
